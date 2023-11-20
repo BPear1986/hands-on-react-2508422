b64e0a159311a8ff6eaae01a885527261fd52100
Brett Pearson <131393660+BPear1986@users.noreply.github.com>
2023-11-20 18:31:51 +0000

    started testing

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,7 +2,7 @@ import React from "react"
 import renderer from "react-test-renderer"
 import App from"../App"
 
-test("App snapshot test", () => {
+it("App snapshot test", () => {
   const component = renderer.create(<App/>);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot()
--- src/__tests__/ListCast.test.js
+++ src/__tests__/ListCast.test.js
@@ -1,3 +1,8 @@
 import React from "react"
 import renderer from "react-test-renderer"
 import ListCast from"../ListCast"
+
+it("renders all cast members", () => {
+  list = renderer.create(<ListCast/>)
+  expect(list.length).toEqual(16)
+})
