07e63b8a1580703cb51c1bfb275a95a6a873e584
Brett Pearson <131393660+BPear1986@users.noreply.github.com>
2023-11-20 18:31:24 +0000

    started testing

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  presets : [
+    '@babel/preset-env',
+    ['@babel/preset-react', {runtime: 'automatic'}],
+  ],
+  plugins:['@babel/plugin-transform-async-to-generator',
+  					'@babel/plugin-transform-regenerator',]
+};
